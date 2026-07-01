d387a5d9ef854fddcde7509faf04122cecab856d
Shubh Mittal <shubhmittal8971@gmail.com>
2026-07-01 18:22:41 +0530

    DAY265

---
+++ src/config.js
@@ -0,0 +1,3 @@
+// src/config.js
+export const SUPABASE_URL = "https://bnckdvybltzwvcspekos.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuY2tkdnlibHR6d3Zjc3Bla29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY0OTgsImV4cCI6MjA5NzUxMjQ5OH0.gKUXFVzTy4Rh21eJjdyuUeDbQkDIHxsxORbNgGKjoG0";
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // TEMPORARY: Paste your actual keys here directly just to test the connection
-const supabaseUrl = "https://bnckdvybltzwvcspekos.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuY2tkdnlibHR6d3Zjc3Bla29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY0OTgsImV4cCI6MjA5NzUxMjQ5OH0.gKUXFVzTy4Rh21eJjdyuUeDbQkDIHxsxORbNgGKjoG0";
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
