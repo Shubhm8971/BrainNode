b41cda3ba96f5e7a97dc2b16ec2815c65e7d76ba
Shubh Mittal <shubhmittal8971@gmail.com>
2026-07-09 21:33:05 +0530

    DAY351

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from './components/ThemeContext.jsx'
-import { AuthProvider } from './AuthContext.jsx' // Make sure the path is correct
+import { AuthProvider } from './AuthContext.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* AuthProvider must wrap the theme and app so everything has access to user data */}
+    {/* 1. AuthProvider is at the root to manage the global session.
+      2. ThemeProvider is nested inside, allowing it to access 
+         the 'user' and 'profile' data from AuthProvider.
+    */}
     <AuthProvider>
       <ThemeProvider>
         <App />
